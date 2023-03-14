4f09f061a1951266ef9c5c1ec0823388622193b5
Marcello Delcaro <67984156+marcello-delcaro@users.noreply.github.com>
2023-03-14 10:26:24 -0700

    Update auto_update.sh
    Changed script to upgrade to the tech preview build of THOR and clean up the output to hide the unzipping/zipping routines

--- auto_update.sh
+++ auto_update.sh
@@ -1,30 +1,25 @@
 #!/bin/bash
-## Place new license into /lambda_functions/analyzer/
 
-## run this script to auto update license from nextron
-
-
+exec 3>&1 &>/dev/null
 # unzip Dependencies.zip to temp_dep/
 mkdir /binaryalert/lambda_functions/analyzer/temp_dep
 unzip /binaryalert/lambda_functions/analyzer/dependencies -d /binaryalert/lambda_functions/analyzer/temp_dep/
 wait
-mv /binaryalert/lambda_functions/analyzer/*.lic /binaryalert/lambda_functions/analyzer/temp_dep/ || echo "No New License File Found"
+mv /binaryalert/lambda_functions/analyzer/*.lic /binaryalert/lambda_functions/analyzer/temp_dep/ || echo "No New License File Found" >&3
 # make thor-util executable
 chmod u+x /binaryalert/lambda_functions/analyzer/temp_dep/thor-util
-# update to latest ruleset using thor-util
-/binaryalert/lambda_functions/analyzer/temp_dep/thor-util update
-wait
 # upgrade to lastest THOR version using thor-util
-/binaryalert/lambda_functions/analyzer/temp_dep/thor-util upgrade
+echo "Launching THOR util to update licenses, YARA rules, and THOR" >&3
+/binaryalert/lambda_functions/analyzer/temp_dep/thor-util update && /binaryalert/lambda_functions/analyzer/temp_dep/thor-util upgrade --techpreview >&3
 wait
 rm -rf /binaryalert/lambda_functions/analyzer/dependencies.zip
 cd /binaryalert/lambda_functions/analyzer/temp_dep/ && zip -ro ../dependencies.zip .
 cd /binaryalert
 rm -rf /binaryalert/lambda_functions/analyzer/temp_dep/
 
-./manage.py apply
+./manage.py apply >&3
 ./manage.py build
 wait
-## ./dashboard.py if custom dashboard created, it will repopulate here
+# if custom dashboard was created it will redeploy it here
 ./dashboard.py
-echo "THOR has been updated"
+echo "THOR analyzer lambdas have been upgraded and redeployed" >&3
